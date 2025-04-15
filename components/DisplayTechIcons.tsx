f42450229ac8a93d9c1257cc516f3984c62a0231
LordRona <ronardsauh@gmail.com>
2025-04-15 05:08:26 +0100

    Added the generative ai page

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -4,15 +4,12 @@ import { cn, getTechLogos } from "@/lib/utils";
 const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
   const techIcons = await getTechLogos(techStack);
 
-  // Add debug logging to verify data
-  console.log("TechIcons data:", techIcons);
 
   return (
     <div className="flex flex-row">
       {techIcons.slice(0, 3).map(({ tech, url }, index) => {
-        // Create a completely unique key
+
         const uniqueKey = `${tech}-${url}-${index}-${Math.random().toString(36).substring(2, 9)}`;
-        console.log(`Rendering tech icon with key: ${uniqueKey}`);
         
         return (
           <div
